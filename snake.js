5b3792b7b5685378244f4995aa24ac325484de97
Rinaldo Nelson <rinaldo.nelson1@gmail.com>
2022-09-20 10:51:27 -0400

    Error Fix

--- snake.js
+++ snake.js
@@ -2,7 +2,9 @@ export const SNAKE_SPEED = 1
 const snakeBody = [
     { x: 10, y: 11 },
     { x: 11, y: 11 },
-    { x: 12, y: 11 }
+    { x: 12, y: 11 },
+    { x: 13, y: 11 },
+    { x: 14, y: 11 }
 ]
 
 export function update() {
