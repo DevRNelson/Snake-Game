a035b19b1264bdbb271aec65b8559c23563425f2
Rinaldo Nelson <rinaldo.nelson1@gmail.com>
2022-09-20 09:49:59 -0400

    Added Snake Speed and Frame Updater to Game.js

--- game.js
+++ game.js
@@ -1,8 +1,14 @@
+let lastRenderTime = 0
+const SNAKE_SPEED = 2
+
 function main(currentTime) {
-    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
     window.requestAnimationFrame(main)
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+
+    console.log('Render')
     lastRenderTime = currentTime
-    console.log(currentTime)
 }
 
 window.requestAnimationFrame(main)
