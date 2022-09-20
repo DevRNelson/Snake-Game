76e66250a5d7be765921699503c5d3add1606223
Rinaldo Nelson <rinaldo.nelson1@gmail.com>
2022-09-20 09:44:25 -0400

    Start of Game JS

--- game.js
+++ game.js
@@ -1,3 +1,8 @@
 function main(currentTime) {
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
     window.requestAnimationFrame(main)
+    lastRenderTime = currentTime
+    console.log(currentTime)
 }
+
+window.requestAnimationFrame(main)
